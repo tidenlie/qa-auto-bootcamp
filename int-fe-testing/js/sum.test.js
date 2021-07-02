f4211be5ef45fdfaee847e4cb5b418dd1632a728
tidenlie <lienat@gmail.com>
2021-07-02 15:54:27 +0300

    Add files via upload

---
+++ int-fe-testing/js/sum.test.js
@@ -0,0 +1,15 @@
+const sum = require('./sum');
+
+test('properly adds two numbers', () => {
+  expect(sum(1, 2)).toBe(3)
+})
+
+
+
+//////////////////////////////////////
+
+/* if (sum(1, 3) === 4) {
+  console.log('passed');
+} else {
+  throw Error;
+} */
